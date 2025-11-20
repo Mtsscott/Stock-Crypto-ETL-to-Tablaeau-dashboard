7f0d64e501f8ad3c5367ca6e1dab421f2d86a2eb
Mtsscott <marshalscott909@gmail.com>
2025-11-19 23:51:37 -0700

    Update __init__.py

--- src/strategies/__init__.py
+++ src/strategies/__init__.py
@@ -1,7 +1,23 @@
-from .base_strategy import BaseStrategy
+"""
+Trading strategy implementations.
+
+All strategies inherit from BaseStrategy and implement the generate_signals() method.
+"""
+from .base_strategy import BaseStrategy, SignalValidator
+from .buy_and_hold import BuyAndHold
 from .sma_crossover import SMACrossover
 from .breakout_strategy import BreakoutStrategy
 from .volatility_strategy import VolatilityBreakout
 from .mean_reversion import BollingerMeanReversion
 from .volume_momentum import VolumeMomentum
-from .buy_and_hold import BuyAndHold
+
+__all__ = [
+    'BaseStrategy',
+    'SignalValidator',
+    'BuyAndHold',
+    'SMACrossover',
+    'BreakoutStrategy',
+    'VolatilityBreakout',
+    'BollingerMeanReversion',
+    'VolumeMomentum'
+]
